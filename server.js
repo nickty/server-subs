049ea0f106b4158f93fc5c862c72f5eb67f16fd4
Mizanur Rahman <nickty.86@gmail.com>
2024-04-05 06:19:09 +0600

    server created and running

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import { readdirSync } from 'fs'
+// import express from 'express'
+// import mongoose from 'mongoose'
+// import cors from 'cors'
+// import { readdirSync } from 'fs'
+
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const { readdirSync } = require('fs')
 
 const moran = require('morgan')
 require('dotenv').config()
@@ -17,7 +22,18 @@ mongoose
   .catch((err) => console.log('DB connection error', err))
 
 //middlewares
+app.use(express.json({ limit: '5mb' }))
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+  })
+)
 
 // routes
+app.get('/api/register', (req, res) => {
+  res.send('response from node server')
+})
 
 //listen
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
